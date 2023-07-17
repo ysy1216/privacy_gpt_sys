5f6cfb80b0caa382eac66ad9e3a83b3081b0200e
Kenelm23 <107390609+Kenelm23@users.noreply.github.com>
2023-07-17 18:07:34 +0800

    Add files via upload

--- predict.py
+++ predict.py
@@ -1,44 +1,50 @@
 # coding: utf-8
 # @File: predict.py
-# @Author: HE D.H.
-# @Email: victor-he@qq.com
-# @Time: 2020/10/10 17:13:57
-# @Description:
-
+import re
+import jieba
 import torch
 from model import BertClassifier
 from transformers import BertTokenizer, BertConfig
-
-
 labels = ["敏感", "不敏感"]
 bert_config = BertConfig.from_pretrained('bert-base-chinese')
-
 # 定义模型
 model = BertClassifier(bert_config, len(labels))
-
 # 加载训练好的模型
-model.load_state_dict(torch.load('models/best_model.pkl', map_location=torch.device('cpu')))
+model.load_state_dict(torch.load('../models/best_model.pkl', map_location=torch.device('cpu')))
 model.eval()
-
 tokenizer = BertTokenizer.from_pretrained('bert-base-chinese')
-
-print('类别')
-while True:
-    text = input('Input: ')
-    token = tokenizer(text, add_special_tokens=True, padding='max_length', truncation=True, max_length=512)
-    input_ids = token['input_ids']
-    attention_mask = token['attention_mask']
-    token_type_ids = token['token_type_ids']
-
-    input_ids = torch.tensor([input_ids], dtype=torch.long)
-    attention_mask = torch.tensor([attention_mask], dtype=torch.long)
-    token_type_ids = torch.tensor([token_type_ids], dtype=torch.long)
-
-    predicted = model(
-        input_ids,
-        attention_mask,
-        token_type_ids,
-    )
-    pred_label = torch.argmax(predicted, dim=1)
-
-    print('Label:', labels[pred_label])
+def classic(query):
+    print("脱敏前:" + query)
+    text = jieba.lcut(query)
+    #print(text)
+    sensitive_words = []
+    for tmp_text in text:
+        token = tokenizer(tmp_text, add_special_tokens=True, padding='max_length', truncation=True, max_length=512)
+        input_ids = token['input_ids']
+        attention_mask = token['attention_mask']
+        token_type_ids = token['token_type_ids']
+        input_ids = torch.tensor([input_ids], dtype=torch.long)
+        attention_mask = torch.tensor([attention_mask], dtype=torch.long)
+        token_type_ids = torch.tensor([token_type_ids], dtype=torch.long)
+        predicted = model(
+            input_ids,
+            attention_mask,
+            token_type_ids,
+            )
+        pred_label = torch.argmax(predicted, dim=1)
+        if labels[pred_label] == "敏感":
+            sensitive_words.append(tmp_text)
+            #print(sensitive_words)
+        #print('Label:', labels[pred_label])
+    print("脱敏后:" + mask_sensitive_info(query, sensitive_words))
+def mask_sensitive_info(text, sensitive_words):
+    #print(sensitive_words)
+    for word in sensitive_words:
+        masked_word = ""
+        print(len(word))
+        for i in range(0, (int)(len(word)/2)):
+            masked_word += "*"
+        masked_sensitive = word[:-((int)(len(word)/2))] + masked_word
+        #print(masked_sensitive)
+        text = re.sub(word, masked_sensitive, text, flags=re.IGNORECASE)
+    return text
---
+++ test/test_classic.py
@@ -0,0 +1,13 @@
+from predict import classic
+
+def main():
+    #paths = "../classic_info.txt"
+    #f = open(paths, encoding="utf-8")
+    #lines = f.readlines()
+    #for line in lines:
+        #print(line)
+        #classic(line)
+    #f.close()
+    classic("15151820301的地址是多少")
+if __name__ == "__main__":
+    main()
